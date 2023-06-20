d90c8b2dfa25b7a4ececae95592aa4e835477441
Amisha Goyal <103890419+20BCE10496@users.noreply.github.com>
2023-06-20 23:14:57 +0530

    Added solution - LeetHub

---
+++ Array Subset of another array - GFG/array-subset-of-another-array.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            long n = Long.parseLong(stt.nextToken());
+            long m = Long.parseLong(stt.nextToken());
+            long a1[] = new long[(int)(n)];
+            long a2[] = new long[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a1[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                a2[i] = Long.parseLong(inputLine1[i]);
+            }
+            
+            
+            Compute obj = new Compute();
+            System.out.println(obj.isSubset( a1, a2, n, m));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+
+class Compute
+{
+    public String isSubset(long a1[], long a2[], long n, long m)
+    {
+        HashMap<Long, Integer> map = new HashMap<>();
+        
+        for (long v: a1)
+            map.put(v, map.getOrDefault(v, 0) + 1);
+            
+        for (long v: a2)
+        {
+            Integer freq = map.get(v);
+            
+            if (freq == null || freq == 0)
+                return "No";
+                
+            map.put(v, freq - 1);
+        }
+        
+        return "Yes";
+    }
+}
