d3caafcd49461caf5a7962c89e10b398b62ab395
Amisha Goyal <103890419+20BCE10496@users.noreply.github.com>
2023-07-14 16:05:28 +0530

    Added solution - LeetHub

---
+++ Implement two stacks in an array - GFG/implement-two-stacks-in-an-array.java
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class TwoStack
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in); 
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			twoStacks g = new twoStacks();
+			int Q = sc.nextInt();
+			while(Q>0)
+			{
+				int stack_no = sc.nextInt();
+				int QueryType = sc.nextInt();
+				
+				
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					if(stack_no == 1)
+					 g.push1(a);
+					else if(stack_no ==2)
+					 g.push2(a);
+				}else if(QueryType == 2)
+				{
+					if(stack_no==1)
+					System.out.print(g.pop1()+" ");
+					else if(stack_no==2)
+					System.out.print(g.pop2()+" ");
+				}
+			
+				Q--;
+			}
+				System.out.println();
+		  T--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+
+
+
+
+
+
+
+
+class twoStacks {
+    int[] arr;
+    int top1;
+    int top2;
+    int size;
+
+    twoStacks() {
+        size = 100;
+        arr = new int[size];
+        top1 = -1;
+        top2 = size;
+    }
+
+    // Method to push an element into the first stack.
+    void push1(int x) {
+        if (top1 < top2 - 1) {
+            top1++;
+            arr[top1] = x;
+        } 
+    }
+
+    // Method to push an element into the second stack.
+    void push2(int x) {
+        if (top1 < top2 - 1) {
+            top2--;
+            arr[top2] = x;
+        } 
+    }
+
+    // Method to pop an element from the first stack.
+    int pop1() {
+        if (top1 >= 0) {
+            int poppedElement = arr[top1];
+            top1--;
+            return poppedElement;
+        } else {
+            return -1;
+        }
+    }
+
+    // Method to pop an element from the second stack.
+    int pop2() {
+        if (top2 < size) {
+            int poppedElement = arr[top2];
+            top2++;
+            return poppedElement;
+        } else {
+            return -1;
+        }
+    }
+}
+
+
