ca612a15a3bccea6bb492a7bd5cc0891f538b78f
Amisha Goyal <103890419+20BCE10496@users.noreply.github.com>
2023-08-08 14:31:06 +0530

    Added solution - LeetHub

--- Value equal to index value - GFG/value-equal-to-index-value.java
+++ Value equal to index value - GFG/value-equal-to-index-value.java
@@ -41,12 +41,14 @@ public class Main {
 class Solution {
     ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
         // code here
-        ArrayList<Integer> a=new ArrayList<Integer>();
+        ArrayList<Integer> a=new ArrayList<>();
         for(int i=0;i<n;i++){
             if(arr[i]==i+1){
                 a.add(i+1);
             }
+            
         }
         return a;
+        
     }
 }
