98ed3c42353b734bc23c24e3da821ddb881e6b63
Amisha Goyal <103890419+20BCE10496@users.noreply.github.com>
2023-08-13 01:48:20 +0530

    Added solution - LeetHub

---
+++ Binary representation - GFG/binary-representation.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N = sc.nextInt();
+
+            Solution ob = new Solution();
+            String ans = ob.getBinaryRep(N);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String getBinaryRep(int N){
+        // code here
+        String s="";
+        while(N>0){
+            s=(N%2)+s;
+            N=N/2;
+        }
+        while(s.length()<30){
+            s="0"+s;
+        }
+        return s;
+    }
+}
