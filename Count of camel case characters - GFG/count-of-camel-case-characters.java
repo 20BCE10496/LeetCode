4cd0595004cbf90ad8b6af04c022f73c77ec5fbb
Amisha Goyal <103890419+20BCE10496@users.noreply.github.com>
2023-08-23 00:25:30 +0530

    Added solution - LeetHub

---
+++ Count of camel case characters - GFG/count-of-camel-case-characters.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = sc.next ();
+    		System.out.println (new Sol().countCamelCase (s));
+        }
+        
+    }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    int countCamelCase (String s)
+    {
+        // your code here      
+        int count=0;
+        for(char c:s.toCharArray()){
+            if(Character.isUpperCase(c)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
